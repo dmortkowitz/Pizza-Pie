f14b0488d7af2e3dca8022d91205a9443bc132fc
dm <dmortkowitz@gmail.com>
2018-08-24 11:41:11 -0700

    Add menu options for pizzas, limited styling.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,12 @@
 // business logic
-function Pizza(crust,pizzaSize,numToppings, topChoice) {
+function Pizza(crust,pizzaSize, topChoice) {
   this.crust = crust;
   this.pizzaSize = pizzaSize;
-  this.numToppings = numTopping;
   this.topChoice = topChoice;
 }
-var crusts = {"regular":0, "glutenFree":+2, "rustic":+1}
-var pizzaSizes = {"small":8, "medium":8, "large":10}
-var topChoices = {"pepperoni":+1, "mushroom"}
+var crusts = {"regular":0, "glutenFree":+2, "rustic":+2}
+var pizzaSizes = {"small":10, "medium":14, "large":18}
+var topChoices = {"cheese":0, "pepperoni":+2, "threeMeat":+5, "vegan":+3, "veggie":+2, "blueMoon":+2, "theDavid":+3}
 
 Pizza.prototype.pizzaprice = function(){
   return crusts[this.crust] + pizzaSizes[this.pizzaSize] + topChoices[this.topChoice]
